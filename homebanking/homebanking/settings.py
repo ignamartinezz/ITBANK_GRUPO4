64c0881ea473ab81d6c0821262ae39855840a391
ignamartinezz <igna.martinezzz@gmail.com>
2022-08-14 22:37:06 -0300

    Base

--- homebanking/homebanking/settings.py
+++ homebanking/homebanking/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Base',
+    'Clientes',
+    'Cuentas',
+    'Login',
+    'Prestamos',
+    'Tarjetas',
 ]
 
 MIDDLEWARE = [
--- homebanking/homebanking/urls.py
+++ homebanking/homebanking/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Base import views as baseView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
