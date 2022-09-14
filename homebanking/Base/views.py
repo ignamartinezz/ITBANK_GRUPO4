b290701f19f2329809f4777886e7825bb8f156e0
ignamartinezz <igna.martinezzz@gmail.com>
2022-09-14 19:42:38 -0300

    Login Hotfx

--- homebanking/Base/views.py
+++ homebanking/Base/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from .forms import RegistroForm
 from .models import Sucursal
 from django.urls import reverse
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, auth
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -20,31 +20,45 @@ def home(request):
 
 def login(request):
     # registro_form = RegistroForm
-
     # if request.method == "POST":
-
     #     registro_form = registro_form(data=request.POST)
-
     #     if registro_form.is_valid():
     #         nombre_user = request.POST.get('registerFirstName', '')
     #         apellido_user = request.POST.get('registerLastName', '')
     #         username_user = request.POST.get('registerUsername', '')
-            
     #         email = request.POST.get('registerEmail', '')
     #         password = request.POST.get('registerPassword', '')
-            
+    
     #         user = User.objects.create_user(username_user,email,password)
     #         user.save()
 
     #         return redirect(reverse('base/signup.html'))
+    if request.method == "POST":
+        #login
+        usernamelogin=request.POST['loginName']
+        passwordlogin=request.POST['loginPassword']
+
+        print(usernamelogin)
+
+        usuariologin= auth.authenticate(username=usernamelogin, password=passwordlogin)
+        print(usuariologin)
+
+        if usuariologin is not None:
+            auth.login(request,usuariologin)
+            return redirect('homeBanking')
+    return render(request, "base/login.html")
+
+
+
+
+def signup(request):
     registro_form = RegistroForm
 
     if request.method == "POST":
 
         registro_form = registro_form(data=request.POST)
-        print("holahola")
-        # nombre_user = request.POST.get('registerFirstName', '')
-        # print(nombre_user)
+                # nombre_user = request.POST.get('registerFirstName', '')
+                # print(nombre_user)
         print(registro_form.errors)
         if registro_form.is_valid():
             nombre_user = request.POST.get('registerFirstName', '')
@@ -60,8 +74,9 @@ def login(request):
             user.save()
 
             return redirect(reverse('login'))
-        
+                
     return render(request, "base/signup.html")
+
 
 
 @login_required(login_url='login')
--- homebanking/homebanking/urls.py
+++ homebanking/homebanking/urls.py
@@ -8,7 +8,9 @@ from Tarjetas import views as TarjetaView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',   baseView.home, name="home"),
-    path('signup/', baseView.login, name="login"),
+    path('login/', baseView.login, name="login"),
+    path('signup/', baseView.signup, name="signup"),
+    
     path('signup/homeBanking', baseView.homeBanking, name="homeBanking"),
     path('homeBanking/', baseView.homeBanking, name="homeBanking"),
     path('prestamo/', PrestamoView.prestamo, name="prestamo"),
